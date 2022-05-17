f4b6ebc60ad4c8fee3dceb7ea6879942e237a9c9
Kamlesh Panwar <kkamlesh286@gmail.com>
2022-05-17 23:55:08 +0530

    update: rating two added

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,7 +10,16 @@ export default function Testimonials() {
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
           Client Testimonials
         </h1>
-        <div><img className="m-auto" alt = "test" src= {'./rating.png'}/></div>
+        <div className=" grid ms:grid-cols-1 gap-4 md:grid-cols-2">
+          <div>
+            <img  alt = "test" src= {'./rating.png'}/>
+          </div>
+          <div>
+            <img  alt = "test" src= {'./ratingtwo.png'}/>
+          </div>
+        </div>
+        {/* <div><img className="grid-cols-1" alt = "test" src= {'./ratingtwo.png'}/></div> */}
+
         {/* <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
             <div className="p-4 md:w-1/2 w-full">
